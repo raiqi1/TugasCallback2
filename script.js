e2981db0465ae6e42b0507b95c8a0ed60baa66db
rayiqi <vindbaiq@gmail.com>
2023-02-19 22:22:04 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const table = document.getElementById('data');
+table.innerHTML = message('loading...');
+const data = fetch('https://jsonplaceholder.typicode.com/users');
+data
+  .then(function(res){ return res.json()})
+  .then(res => {
+      table.innerHTML = render(res)
+  })
+  .catch(err => {
+      table.innerHTML = message(err.message)
+  })
+  .finally(() => {
+      
+  });
+
+
+
+function render(result) {
+  let table = '';
+  result.forEach(data => {
+    table += `<tr>
+                <td>${data.id}</td>
+                <td>${data.name}</td>
+                <td>${data.username}</td>
+                <td>${data.email}</td>
+                <td>
+                  ${data.address.street},
+                  ${data.address.suite}, 
+                  ${data.address.city}
+                 </td>
+                <td>${data.phone}</td>
+                <td>${data.website}</td>
+                <td>${data.company.name}</td>
+              </tr>`;
+  })
+  return table;
+} 
+
+function message(msg) {
+  return `<tr>
+            <td class="text-center" colspan="8">${msg}</td>
+        </tr>`;
+}
